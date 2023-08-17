a73c62bd88bf7c38a016c5ad76805654dcaf8679
Diwakar Sharma <diwakarchiku12@gmail.com>
2023-08-17 12:57:59 +0530

    thrice 6th Commit

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -21,13 +21,41 @@ public class Car extends Vehicle {
         this.currentGear = 1;
     }
 
+    public int getWheels() {
+        return wheels;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
     public void changeGear(int newGear){
         this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-        super.move(newSpeed, newDirection);
+        move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
